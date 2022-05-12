89162ef484a2b03c722b550e1fed4e7d2c99dc25
Yanek K <yanek.dev@gmail.com>
2022-05-12 14:32:19 -0400

    Ex 1.5 complete

--- part_1/halfstack_app/src/Content.js
+++ part_1/halfstack_app/src/Content.js
@@ -2,11 +2,12 @@ import React from "react";
 import Part from "./Part";
 
 const Content = (props) => {
+  const [part1, part2, part3] = props.parts;
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={part1.name} exercises={part1.exercises} />
+      <Part part={part2.name} exercises={part2.exercises} />
+      <Part part={part3.name} exercises={part3.exercises} />
     </div>
   );
 };
--- part_1/halfstack_app/src/Total.js
+++ part_1/halfstack_app/src/Total.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Total = (props) => {
+  const [part1, part2, part3] = props.parts;
   return (
     <div>
       <p>
         Number of exercises :{" "}
-        {props.exercises1 + props.exercises2 + props.exercises3}
+        {part1.exercises + part2.exercises + part3.exercises}
       </p>
     </div>
   );
