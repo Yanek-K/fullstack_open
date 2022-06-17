5b7c614c61e382713c3d951a296b7ed94d5a430d
Yanek K <yanek.dev@gmail.com>
2022-06-17 18:40:48 -0400

    Changed baseurl to relative URL for backendbuild

--- part_2/phonebook/src/services/backend.js
+++ part_2/phonebook/src/services/backend.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "https://phonebook-fullstack-open12.herokuapp.com/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
