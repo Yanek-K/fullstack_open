d031f494b617a8f25885f221e95c3115a82f16a7
Yanek K <yanek.dev@gmail.com>
2022-07-22 18:40:39 -0400

    Refactor almost Complete

--- part_4/blog_list/controllers/blogs.js
+++ part_4/blog_list/controllers/blogs.js
@@ -1,2 +1,32 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
+
+blogRouter.get('/home', (request, response) => {
+  response.send('<h2>You have made it this far<h2>');
+});
+
+blogRouter.get('/', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+blogRouter.post('/', (request, response) => {
+  const body = request.body;
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
+
+  blog
+    .save()
+    .then((savedBlog) => {
+      response.json(savedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = blogRouter;
--- part_4/blog_list/index.js
+++ part_4/blog_list/index.js
@@ -6,20 +6,13 @@ const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
 
+const blogRouter = require('./controllers/blogs');
+
 const server = http.createServer(app);
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 const mongoUrl = process.env.MONGODB_URI;
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
 
 console.log('Connecting to Database');
 mongoose
@@ -31,23 +24,7 @@ mongoose
     console.log('Error connecting to MongoDB:', error.message);
   });
 
-app.get('/', (request, response) => {
-  response.send('<h1>NICE</h1>');
-});
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use('/api/blogs', blogRouter);
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on Port ${config.PORT}`);
