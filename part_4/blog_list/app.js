f279455bc685219af3eb1027238f742d92755121
Yanek K <yanek.dev@gmail.com>
2022-08-02 22:38:15 -0400

    Ex 4.18 Complete

--- part_4/blog_list/app.js
+++ part_4/blog_list/app.js
@@ -7,6 +7,7 @@ const blogRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
+const loginRouter = require('./controllers/login');
 const mongoose = require('mongoose');
 
 logger.info('Connecting to MongoDB');
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogRouter);
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
